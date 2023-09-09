08cc260356cc4f47b1ed1dccac569e416eba82b0
João Moreira <jgmoreira1809@gmail.com>
2023-09-09 14:45:44 -0300

    formating title

--- src/Components/EnglishTitle.js
+++ src/Components/EnglishTitle.js
@@ -3,7 +3,9 @@ import React from 'react';
 export default function EnglishTitle() {  
     return (
     <>
-        <h1>Hello, my name is João Moreira!</h1>
+        <h1>Hello, my name is
+        <br/>
+        João Moreira!</h1>
         <h2>I&apos;m going to tell you a little bit about me
         <br/>
         and show you my main projects as a
--- src/Components/PortugueseTitle.js
+++ src/Components/PortugueseTitle.js
@@ -3,7 +3,9 @@ import React from 'react';
 export default function PortugueseTitle() {
     return (
       <>
-        <h1>Olá, meu nome é João Moreira!</h1>
+        <h1>Olá, meu nome é 
+        <br/>
+        João Moreira!</h1>
         <h2>Aqui eu vou contar um pouco sobre mim
         <br/>
         e mostrar meus principais projetos como
