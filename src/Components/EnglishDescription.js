29d7912298886c671e2067513bb0fe4ae5fedd49
João Moreira <jgmoreira1809@gmail.com>
2023-10-16 15:43:03 -0300

    Photo adding

--- src/Components/EnglishDescription.js
+++ src/Components/EnglishDescription.js
@@ -1,19 +1,26 @@
 import React from 'react';
+const meAndSammy = require('../Images/me_and_sammy.jpg');
+
 
 export default function EnglishDescription() {
     return (
       <div className='aboutMe'>
-        <h4>About me</h4>  
-        <br/>    
-        <ul>
-          <li>I was born in Rio de Janeiro and I&apos;m Samuel&apos;s father!</li>
-        <br/>
-          <li>I am a certified freelance software developer through Trybe, and I strive to improve my skills every day.</li>
-        <br/>
-          <li>Currently, I am a Software Engineering student, aiming to solidify all the practical knowledge I have.</li>
-        <br/>
-          <li>I am a weekend musician.</li>
-        </ul>
+        <div className='text'>
+          <h4>About me</h4>  
+          <br/>    
+          <ul>
+            <li>I was born in Rio de Janeiro and I&apos;m Samuel&apos;s father!</li>
+          <br/>
+            <li>I am a certified freelance software developer through Trybe, and I strive to improve my skills every day.</li>
+          <br/>
+            <li>I am a Software Engineering student, aiming to solidify all the practical knowledge I have.</li>
+          <br/>
+            <li>I work as a musician on weekends.</li>
+          </ul>
+        </div>
+        <div className='photo'>
+          <img className='me_and_sammy' alt='Me and Sammy' src={meAndSammy}/>
+        </div>
       </div>
     )
 }
--- src/Components/PortugueseDescription.js
+++ src/Components/PortugueseDescription.js
@@ -1,18 +1,24 @@
 import React from 'react';
+const meAndSammy = require('../Images/me_and_sammy.jpg');
 
 export default function PortugueseDescription() {
     return (
         <div className='aboutMe'>
+          <div className='text'>
             <h4>Sobre mim</h4>
             <ul>
               <li>Nasci no Rio de Janeiro e sou pai do Samuel!</li>
             <br/>
               <li>Sou desenvolvedor de software freelance certificado pela Trybe e busco me aperfeiçoar todos os dias.</li>
             <br/>     
-              <li>Atualmente sou aluno de Engenharia de Software buscando embasar todo o conhecimento prático que possuo.</li>
+              <li>Estou estudando Engenharia de Software buscando embasar todo o conhecimento prático que possuo.</li>
             <br/>
               <li>Músico nos finais de semana</li>
             </ul>
+          </div>
+          <div className='photo'>
+            <img className='me_and_sammy' alt='Me and Sammy' src={meAndSammy}/>
+          </div>
         </div>
     )
 }
